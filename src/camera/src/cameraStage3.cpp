33b044c0dcbb648e1c1ff4d267c3412862debe4d
fu941110 <fu941110@gapp.nthu.edu.tw>
2025-08-07 16:20:00 +0800

    Update cameraStage3.cpp

--- src/camera/src/cameraStage3.cpp
+++ src/camera/src/cameraStage3.cpp
@@ -7,17 +7,17 @@ using namespace std;
 class CameraStage3 : public Camera {
 public:
     CameraStage3() : Camera() {
-        setTrackbarPosition(orange_min_hue, orange_max_hue, 
-                            orange_min_sat, orange_max_sat, 
-                            orange_min_val, orange_max_val); 
+        // setTrackbarPosition(orange_min_hue, orange_max_hue, 
+        //                     orange_min_sat, orange_max_sat, 
+        //                     orange_min_val, orange_max_val); 
     }
 
 
     Mat FindOrange(Mat img) {
         Mat hsv, mask, result;
         cvtColor(img, hsv, COLOR_BGR2HSV);
-        inRange(hsv, Scalar(hue_min_, sat_min_, val_min_), 
-                Scalar(hue_max_, sat_max_, val_max_),mask);
+        inRange(hsv, Scalar(orange_min_hue, orange_min_sat, orange_min_val), 
+                Scalar(orange_max_hue, orange_max_sat, orange_max_val),mask);
         // inRange(hsv, Scalar(orange_min_hue, orange_min_sat, orange_min_val), 
         //         Scalar(orange_max_hue, orange_max_sat, orange_max_val), mask);
         bitwise_and(img, img, result, mask);
@@ -40,7 +40,7 @@ public:
             orange_center_x = static_cast<int>(m.m10 / m.m00) - (img.cols / 2);
             orange_center_y = static_cast<int>(m.m01 / m.m00) - (img.rows / 2);
             Scalar PointColor = (abs(orange_center_x) + abs(orange_center_y) > 50) ? Scalar(0, 0, 255) : Scalar(0, 255, 0); 
-            circle(result, Point(orange_center_x + img.cols/2, orange_center_y + img.rows/2), 10, PointColor, -1);
+            // circle(result, Point(orange_center_x + img.cols/2, orange_center_y + img.rows/2), 10, PointColor, -1);
             printf("Orange Center: (%d, %d), Area: %d\n", orange_center_x, orange_center_y, orange_area);
         }
 
@@ -74,10 +74,10 @@ int main() {
             std::cout << "can't receive frame\n";
             break;
         }
-        imshow("image", img); //初始
+        // imshow("image", img); //初始
 
         orange = camera3.FindOrange(img);
-        imshow("Orange Detection", orange); // 橘子檢測結果
+        // imshow("Orange Detection", orange); // 橘子檢測結果
 
 
         if (waitKey(1) == 27) break;
