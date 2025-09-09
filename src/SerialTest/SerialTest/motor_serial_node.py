0df8be40a5da3b0a8921c58c312322da4fe2b4d5
fu941110 <fu941110@gapp.nthu.edu.tw>
2025-09-09 22:59:00 +0800

    Update motor_serial_node.py

--- src/SerialTest/SerialTest/motor_serial_node.py
+++ src/SerialTest/SerialTest/motor_serial_node.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
+from rclpy.executors import MultiThreadedExecutor
+from rclpy.executors import SingleThreadedExecutor
 from std_msgs.msg import String
 from mainspace.msg import Position, ToStmSpeed, Coffee, Pause
 import serial
@@ -13,23 +15,26 @@ class MotorSerialNode(Node):
         super().__init__('motor_serial_node')
         self.lock = threading.Lock()
         self.ack_queue = queue.Queue()
-        self.tx_queue = queue.PriorityQueue()
+        # self.tx_queue = queue.PriorityQueue()
+        self.speed_queue = None
+        self.coffee_queue = None
+        self.pause_queue = None
 
         self.pending_cmd = None       
         self.pending_time = 0.0       
-        self.ack_timeout = 0.3
+        self.ack_timeout = 1.0
 
-        self.timer = self.create_timer(0.05, self.timer_callback)
+        self.timer = self.create_timer(0.1, self.timer_callback)
 
         # Position publisher（STM32 ⇒ ROS2）
         self.position_publisher = self.create_publisher(Position, 'stm_position', 10)
 
-        self.speed_subscription = self.create_subscription(
-            ToStmSpeed,
-            'motor_speed',
-            self.speed_callback,
-            10
-        )
+        # self.speed_subscription = self.create_subscription(
+        #     ToStmSpeed,
+        #     'motor_speed',
+        #     self.speed_callback,
+        #     10
+        # )
         self.coffee_subscription = self.create_subscription(
             Coffee,
             'coffee',
@@ -46,7 +51,16 @@ class MotorSerialNode(Node):
 
         # 開啟串列埠
         try:
-            self.ser = serial.Serial('/dev/ttyACM0', 115200, timeout=0.1)
+            self.ser = serial.Serial(
+                '/dev/ttyACM0',
+                115200,
+                timeout=0.01,
+                rtscts=False,
+                dsrdtr=False,
+                xonxoff=False
+            )
+            self.ser.dtr = False
+            self.ser.rts = False
             self.get_logger().info('Serial port opened: /dev/ttyACM0')
             self.running = True
             self.reader_thread = threading.Thread(target=self.uart_reader_thread, daemon=True)
@@ -84,16 +98,25 @@ class MotorSerialNode(Node):
                 self.get_logger().error(f"UART read exception: {e}")
 
     def speed_callback(self, msg):
-        data = f"Speed:{msg.vx:.4f},{msg.vy:.4f},{msg.w:.4f}\n"
-        self.tx_queue.put((3, data))
+        data = f"V:{msg.vx:.4f} {msg.vy:.4f} {msg.w:.4f}\n"
+        with self.lock:
+            if self.speed_queue is None:
+                self.speed_queue = data
+                self.pending_time = 0
 
     def coffee_callback(self, msg):
-        data = f"Coffee:{msg.type},{msg.number}\n"
-        self.tx_queue.put((1, data))
+        data = f"C:{4 * (msg.type) + msg.number}\n"
+        with self.lock:
+            if self.coffee_queue is None:
+                self.coffee_queue = data
+                self.pending_time = 0
 
     def pause_callback(self, msg):
-        data = f"Pause:{msg.pause}\n"
-        self.tx_queue.put((0, data)) 
+        data = f"P:{int(msg.pause)}\n"
+        with self.lock:
+            if self.pause_queue is None:
+                self.pause_queue = data
+                self.pending_time = 0
 
     # ----------- Timer 統一處理 UART ----------
     def timer_callback(self):
@@ -101,31 +124,32 @@ class MotorSerialNode(Node):
         try:
             with self.lock:
                 # --- 如果有 pending CMD，先檢查 ACK ---
-                if self.pending_cmd:
+                if self.pause_queue:
                     if not self.ack_queue.empty():
                         self.ack_queue.get_nowait()   
                         self.get_logger().info("ACK received, CMD done")
-                        self.pending_cmd = None       
+                        self.pause_queue = None
                     elif now - self.pending_time > self.ack_timeout:
-                        self.ser.write(self.pending_cmd.encode('utf-8'))
+                        self.ser.write(self.pause_queue.encode('utf-8'))
+                        self.ser.flush()
                         self.pending_time = now
-                        self.get_logger().warn(f"Resent CMD: {self.pending_cmd.strip()}")
-                    return
-
-                if not self.tx_queue.empty():
-                    priority, data = self.tx_queue.get_nowait()
-                    if data.startswith("Coffee:") or data.startswith("Pause:"):
-                        self.ser.write(data.encode('utf-8'))
-                        self.pending_cmd = data
+                        self.get_logger().warn(f"Resent CMD: {self.pause_queue.strip()}")
+                elif self.coffee_queue:
+                    if not self.ack_queue.empty():
+                        self.ack_queue.get_nowait()   
+                        self.get_logger().info("ACK received, CMD done")
+                        self.coffee_queue = None
+                    elif now - self.pending_time > self.ack_timeout:
+                        self.ser.write(self.coffee_queue.encode('utf-8'))
+                        self.ser.flush()
                         self.pending_time = now
-                        self.get_logger().info(f"Sent CMD: {data.strip()} (waiting for ACK)")
-                    else:
-                        # POS → 直接送，不等 ACK
-                        self.ser.write(data.encode('utf-8'))
+                        self.get_logger().warn(f"Resent CMD: {self.coffee_queue.strip()}")
+                elif self.speed_queue:
+                    self.ser.write(self.speed_queue.encode('utf-8'))
+                    self.ser.flush()
 
         except Exception as e:
             self.get_logger().error(f"Serial write error: {e}")
-
             
     def destroy_node(self):
         self.running = False
@@ -134,7 +158,7 @@ class MotorSerialNode(Node):
         super().destroy_node()
             
 def main(args=None):
-    print("[motor_serial_node] Starting node...")
+    print("[mission_serial_node] Starting node...")
     rclpy.init(args=args)
     node = MotorSerialNode()
     rclpy.spin(node)
